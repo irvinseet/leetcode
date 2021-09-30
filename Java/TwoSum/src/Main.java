30fa250780ca4b97f4cc8137f0acdce3d10804b0
irvinseet <irvinseet@gmail.com>
2021-10-01 00:05:33 +0800

    twoSum

---
+++ Java/TwoSum/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+
+    //this program returns the index of two numbers in the array whose sum is equal to target value
+    public static void main(String[] args) {
+
+        int[] data0 = {2,7,11,15};
+        System.out.println(Arrays.toString(Solution.twoSum(data0, 9)));
+    }
+}
